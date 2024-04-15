61a4a6b5236bc5535382a5a639d75b3112a14ed1
bestgoogoo <junta9996@gmail.com>
2024-04-15 20:59:35 +0900

    Server action & form data validation in sms  - With a library called a validator  - Unlike before, Using preState 🚀

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,22 +1,42 @@
-import FormButton from "@/components/Button";
-import FormInput from "@/components/Input";
+"use client";
+
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import { useFormState } from "react-dom";
+import { smsLogIn } from "./actions";
+import { error } from "console";
+
+const initialState = {
+  token: false,
+  error: undefined,
+};
 
 export default function SMSLogIn() {
+  const [state, dispatch] = useFormState(smsLogIn, initialState);
   return (
     <div className="flex flex-col gap-10">
       <div className="flex flex-col gap-2">
         <h2>Hello!</h2>
         <h3>Log in with your phone number!</h3>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="tel" placeholder="Phone number" required errors={[]} />
-        <FormInput
-          type="password"
-          placeholder="Verification code"
-          required
-          errors={[]}
-        />
-        <FormButton text="Verify" loading={false} />
+      <form action={dispatch} className="flex flex-col gap-3">
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "Verify" : "Request Verification Code"} />
       </form>
     </div>
   );
